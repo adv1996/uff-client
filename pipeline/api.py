c851dbc8af5dd79ada12941e82fc71cecd7292a9
Advaith Venkatakrishnan <advaith96@gmail.com>
2022-12-12 22:47:57 -0500

    feat(pipeline): seed transactions and draft

--- pipeline/api.py
+++ pipeline/api.py
@@ -18,18 +18,26 @@ def collateData():
         db[f'settings-{leagueId}'] = data['settings']
         db[f'users-{leagueId}'] = data['users']
         db[f'rosters-{leagueId}'] = data['rosters']
-        
+        db[f'draft-{leagueId}'] = data['draft']
+
         routes[f'/api/league/{leagueId}'] = f'/settings-{leagueId}'
         routes[f'/api/league/{leagueId}/users'] = f'/users-{leagueId}'
         routes[f'/api/league/{leagueId}/rosters'] = f'/rosters-{leagueId}'
+        routes[f'/api/draft/{leagueId}/picks'] = f"/draft-{leagueId}"
         
-        # Add Matchup API Routes
+        # Add Matchup and Transactions API Routes
         matchups = list(filter(lambda x: "matchup" in x, keys))
+        transactions = list(filter(lambda x: "transaction" in x, keys))
         
         for matchup in matchups:
             db[f'{leagueId}-{matchup}'] = data[matchup]
 
+        for transaction in transactions:
+            db[f'{leagueId}-{transaction}'] = data[transaction]
+
         routes[f'/api/league/{leagueId}/matchups/*'] = f"/{leagueId}-matchup_$1"
+        routes[f'/api/league/{leagueId}/transactions/*'] = f"/{leagueId}-transactions_$1"
+
             
     saveToJson('./api/db.json', db)
     saveToJson('./api/routes.json', routes)
